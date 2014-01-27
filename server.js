e9dfa51c57d8a4ea2910057f470bf64190dde2ab
Densaugeo <nathan.yinger@gmail.com>
2014-01-26 19:42:11 -0500

    server.js changes

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ var ws = require('ws');
 // HTTP server //
 /////////////////
 
-var FILE_PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var IP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var app = express();
 var httpServer = http.createServer(app);
-httpServer.listen(FILE_PORT, IP);
+httpServer.listen(PORT, IP);
 
 
 // Simple static page server
 app.use('/http', express.static('./http'));
 app.use(express.compress());
-console.log((new Date()) + ': Static file server listening at http://' + IP + ':' + FILE_PORT + '/http');
+console.log(new Date().toUTCString() + ': Static file server listening at http://' + IP + ':' + PORT + '/http');
 
 ///////////////
 // WS server //
@@ -28,7 +28,11 @@ console.log((new Date()) + ': Static file server listening at http://' + IP + ':
 var websocket = new ws.Server({server: httpServer, path: '/ws'});
 
 websocket.on('connection', function(connection) {
-  console.log((new Date()) + ': Received connection from: ' + '?');
+  console.log(new Date().toUTCString() + ': Received WebSocket');
+  
+  connection.on('close', function() {
+    console.log(new Date().toUTCString() + ': Closed WebSocket');
+  });
 });
 
 /////////
