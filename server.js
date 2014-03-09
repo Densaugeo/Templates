33f028f3dcf987af239cd992bfc02c5e2eb59f46
Densaugeo <nathan.yinger@gmail.com>
2014-03-09 19:33:49 -0400

    Allow setting options CLI and config files

--- server.js
+++ server.js
@@ -1,33 +1,79 @@
 process.title = 'Template';
 
-var http = require('http');
-var repl = require('repl');
+var http    = require('http');
+var repl    = require('repl');
+var fs      = require('fs');
 var express = require('express');
-var ws = require('ws');
+var ws      = require('ws');
+var stdio   = require('stdio');
+var iz      = require('iz');
+
+/////////////////
+// Get options //
+/////////////////
+
+var optionsCLI = stdio.getopt({
+  'ip'    : {key: 'i', args: 1, description: 'IP address for both HTTP and WS servers. Defaults to OpenShift if available, or if not then 0.0.0.0'},
+  'port'  : {key: 'p', args: 1, description: 'TCP port for both HTTP and WS servers. Defaults to OpenShift if available, or if not then 8080'},
+  'config': {key: 'c', args: 1, description: 'Load settings from configurations file. Defaults to ./config.json'},
+  'nofile': {          args: 0, description: 'Run without a config file. "ip" and "port" options must be specified'}
+});
+
+try {
+  var optionsFile = JSON.parse(fs.readFileSync(optionsCLI.config || 'config.json'));
+}
+catch(error) {
+  console.error('Warning: Unable to read config file "' + (optionsCLI.config || 'config.json') + '". (' + error + '). Options not specified by argument will fall back to hard-coded defaults');
+  var optionsFile = {}
+}
+
+var optionsDefault = {
+  ip    : '0.0.0.0',
+  port  : 8080,
+  config: null,
+  nofile: true
+};
+
+var options = {};
+
+for(var i in optionsDefault) {
+  options[i] = optionsCLI[i] || optionsFile[i] || optionsDefault[i];
+}
+
+if(!iz(options.ip).ip().valid) {
+  console.error('Error: IP must be a valid IP address or local url');
+  process.exit(1);
+}
+
+if(!iz(options.port).int().between(1, 65535).valid) {
+  console.error('Error: TCP port must be an integer between 1 and 65535');
+  process.exit(1);
+}
+
+if(iz(options.port).int().between(1, 1024).valid) {
+  console.warn('Warning: TCP ports between 1 and 1024 may require root permission');
+}
 
 /////////////////
 // HTTP server //
 /////////////////
 
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var IP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var app = express();
 var httpServer = http.createServer(app);
-httpServer.listen(PORT, IP);
-
+httpServer.listen(options.port, options.ip);
 
 // Simple static page server
 app.use('/http', express.static('./http'));
 app.use(express.compress());
-console.log(new Date().toUTCString() + ': Static file server listening at http://' + IP + ':' + PORT + '/http');
+console.log(new Date().toUTCString() + ': Static file server listening at http://' + options.ip + ':' + options.port + '/http');
 
 ///////////////
 // WS server //
 ///////////////
 
-var websocket = new ws.Server({server: httpServer, path: '/ws'});
+var wsServer = new ws.Server({server: httpServer, path: '/ws'});
 
-websocket.on('connection', function(connection) {
+wsServer.on('connection', function(connection) {
   console.log(new Date().toUTCString() + ': Received WebSocket');
   
   connection.on('close', function() {
@@ -40,4 +86,17 @@ websocket.on('connection', function(connection) {
 /////////
 
 var cli = repl.start({});
-cli.context.app = app;
+cli.context.http           = http;
+cli.context.repl           = repl;
+cli.context.fs             = fs;
+cli.context.express        = express;
+cli.context.ws             = ws;
+cli.context.stdio          = stdio;
+cli.context.iz             = iz;
+cli.context.optionsCLI     = optionsCLI;
+cli.context.optionsFile    = optionsFile;
+cli.context.optionsDefault = optionsDefault;
+cli.context.options        = options;
+cli.context.app            = app;
+cli.context.httpServer     = httpServer;
+cli.context.wsServer       = wsServer;
